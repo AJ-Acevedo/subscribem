6719eb76e5f3469440f2bbb945f751443b9492a0
AJ Acevedo <aj@ajalabs.com>
2013-06-08 14:51:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Multitenancy with Rails
+
+And subscriptions too!
+
+Ryan Bigg
+
+This repo follows along with the book Multitenancy with Rails  
+by Ryan Bigg [http://leanpub.com/multi-tenancy-rails](http://leanpub.com/multi-tenancy-rails)
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '2c050327e90092ef9a47689ab82c04c577cb5be6c7a0b9225420bea762e1b7e73dff2ff867c0ac510484a356707a3ca8f22d2b74a7266abb278b378398aeb859'
